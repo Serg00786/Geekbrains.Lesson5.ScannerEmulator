2e1ec1e8f43814224503ba362724a822a158e7aa
Serg <a_s_v86@mail.ru>
2021-11-07 11:56:58 +0300

    Добавьте файлы проекта.

---
+++ PCEfficiency.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geekbrains.Lesson5.ScannerEmulator
+{
+    class PCEfficiency : IPCEfficiencycs
+    {
+        Random rnd = new Random();
+
+        public int GetCPULOad()
+        {
+            return rnd.Next(1, 100);
+        }
+
+        public int GetMemoryLoad()
+        {
+            return rnd.Next(1, 4096);
+        }
+    }
+}
